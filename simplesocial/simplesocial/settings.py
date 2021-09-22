39366575afe543ec22c60e24b1b110ef65cc35a1
kito-m <kitomiddleton@gmail.com>
2021-09-21 23:34:44 -0400

    2nd commit

--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap4',
     'accounts'
 ]
 
@@ -124,6 +125,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+
+
+LOGIN_REDIRECT_URL = 'test'
+LOGOUT_REDIRECT_URL = 'thanks'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .views import HomePage, TestPage, ThanksPage
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomePage.as_view(), name='home'),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('test/', TestPage.as_view(), name='test'),
+    path('thanks/', ThanksPage.as_view(), name='thanks'),
 ]
